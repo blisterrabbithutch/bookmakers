f7642ea11f1ff974375301c246d7713abcd92fe2
Denis Chizh <denischizh@divandesign.ru>
2018-01-28 03:47:07 +0500

    Сделан пиксель-перфект Страниц: Новости, Прогнозы, Матчи. Исправлен скрипт работы вкладок на странице матчей + создание механизма переключение состояния вкладки.

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ $('.mainSection-content-block').on('click', function(){
     if ($(this).hasClass('checked')) {
         $(this).children('table').fadeOut();
     } else {
-        $(this).toggleClass('checked');
         $(this).children('table').fadeIn();
     }
+    $(this).toggleClass('checked');
+    $(this).find('.mainSection-header-expandIcon').toggleClass('mainSection-header-expandIcon_opened');
 })
 
 $('.filterSectionItem_currency').on('click', function(){
