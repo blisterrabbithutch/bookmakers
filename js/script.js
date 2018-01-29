51178c8f11ac3c15311b2639f29cb6f348cb4492
Denis Chizh <denischizh@divandesign.ru>
2018-01-29 20:12:56 +0500

    Разработка главной страницы. Реализован функционал слайдера на главном экране главной странице.

--- js/script.js
+++ js/script.js
@@ -38,6 +38,23 @@ $('.filterSectionItem_safety').on('click', function(){
 $(document).ready(function(){
     $('.mainSlider-carousel').slick({
         // autoplay: true,
-        // autoplaySpeed: 5000,
+        // autoplaySpeed: 5000, 
+        dots: true
+    });
+
+    function $replaceDots() {
+        $( ".slick-dots li:nth-child(1) button" ).replaceWith( "<div data-item='1' class='mainSlider-matchTitle'>Карточки, голы, стычки. В&nbsp;матче&nbsp;«Ницца» — «Монако» будет&nbsp;всe</div>" );
+        $( ".slick-dots li:nth-child(2) button" ).replaceWith( "<div data-item='2' class='mainSlider-matchTitle'>Paddy Power: Кого подпишут «шпоры» в зимнее окно</div>" );
+        $( ".slick-dots li:nth-child(3) button" ).replaceWith( "<div data-item='3' class='mainSlider-matchTitle'>СК Армении проводит расследование по договорному матчу национального чемпионата</div>" );
+        $( ".slick-dots li:nth-child(4) button" ).replaceWith( "<div data-item='4' class='mainSlider-matchTitle'>Роберто Манчини заинтересовался работой со сборной Австралии на&nbsp;ЧМ–2018</div>" );
+    }
+
+    $replaceDots();
 });
-});
+
+
+$('.mainSlider-matchTitle').on('click', function(){
+    var slide = $(this).attr('data-item');
+    console.log(slide);
+})
+
