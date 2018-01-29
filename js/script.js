94ef9e677f8aad9860064d3e52ac223078cf3d54
Denis Chizh <denischizh@divandesign.ru>
2018-01-29 15:13:23 +0500

    Разработка главной страницы. сделана разметка и базовая стилизация страницы.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,16 @@ $('.mainSection-content-block').on('click', function(){
     $(this).find('.mainSection-header-expandIcon').toggleClass('mainSection-header-expandIcon_opened');
 })
 
+$('.mainMatchesSection-content-block').on('click', function(){
+    if ($(this).hasClass('checked')) {
+        $(this).children('table').fadeOut();
+    } else {
+        $(this).children('table').fadeIn();
+    }
+    $(this).toggleClass('checked');
+    $(this).find('.mainMatchesSection-header-expandIcon').toggleClass('mainSection-header-expandIcon_opened');
+})
+
 $('.filterSectionItem_currency').on('click', function(){
     $('.filterSectionItem-hiddenContent_currency').slideToggle();
     $(this).find('.filterSection-expandIcon').toggleClass('filterSection-expandIcon_opened');
