5ee10ec2eba573f1569d2358021749f218500ad7
Denis Chizh <denischizh@divandesign.ru>
2018-02-02 04:48:01 +0500

    чистка кода.

--- gulpfile.js
+++ gulpfile.js
@@ -36,15 +36,6 @@ gulp.task("copy", function() {
         .pipe(gulp.dest("build"));
 });
 
-// gulp.task('sass', function () {
-//   return gulp.src('./sass/**/*.sass')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./css'));
-// });
- 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.sass', ['sass']);
-// });
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.sass')
@@ -55,12 +46,6 @@ gulp.task('sass', function () {
                 sort: true
             })
         ))
-        // .pipe(postcss([
-        //     autoprefixer({
-        //         browsers: ["last 2 versions"]
-        //     })
-        // ]))
-        // .pipe(autoprefixer())
         .pipe(gulp.dest("css"))
         .pipe(server.stream())
         .pipe(minify())
